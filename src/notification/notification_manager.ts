4a463ae8529897476b3c198044db997c5681f261
Moritz <moritz@bachmaiers.de>
2024-03-16 12:47:40 +0100

    edit getClient for notification send

--- src/notification/notification_manager.ts
+++ src/notification/notification_manager.ts
@@ -20,7 +20,7 @@ export class NotificationManager {
         const match: RegExpMatchArray | null = event.objectID.match("(hiob.\\d*.devices.)(.*)(.sendNotification)");
         if (match && match[2] && !event.ack) {
             const deviceID = match[2];
-            const client = this.adapter.server?.getClient(deviceID);
+            const client = this.adapter.server?.getClient(deviceID, true);
             //Check if client is connected
             if (client?.isConnected) {
                 this.sendNotificationLocal(client, event.value);
