271a74550c2757ddb2af2ac86c90b5aeb8f97906
Moritz <msg@bachmaiers.de>
2024-12-03 18:10:24 +0100

    turned off jsdoc rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -25,7 +25,7 @@ export default [
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
         // as this improves maintainability. jsdoc warnings will not block buiuld process.
         rules: {
-            // 'jsdoc/require-jsdoc': 'off',
+            'jsdoc/require-jsdoc': 'off',
             "quotes": ["error", "double"] // Enforce double quotes
             
         },
