574e7e46ba847ccc5ec25b52eaa3a813314ee017
Moritz <msg@bachmaiers.de>
2026-08-11 12:30:11 +0200

    feat: integrate dotenv for environment variable management in Supabase config

--- build/server/supabase/supabase-config.js
+++ build/server/supabase/supabase-config.js
@@ -1,7 +1,9 @@
 "use strict";
+var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
   for (var name in all)
@@ -15,6 +17,14 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var supabase_config_exports = {};
 __export(supabase_config_exports, {
@@ -22,8 +32,10 @@ __export(supabase_config_exports, {
   SUPABASE_URL: () => SUPABASE_URL
 });
 module.exports = __toCommonJS(supabase_config_exports);
-const SUPABASE_URL = "http://127.0.0.1:54321";
-const SUPABASE_ANON_KEY = "sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH";
+var dotenv = __toESM(require("dotenv"));
+dotenv.config();
+const SUPABASE_URL = process.env.SUPABASE_URL || "https://chvazplrvwsvznegekqy.supabase.co";
+const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "sb_publishable_3dW4LAbXNar4lLW8iyOLKQ_fucrNUi-";
 // Annotate the CommonJS export names for ESM import in node:
 0 && (module.exports = {
   SUPABASE_ANON_KEY,
--- src/server/supabase/supabase-config.ts
+++ src/server/supabase/supabase-config.ts
@@ -1,4 +1,6 @@
-//export const SUPABASE_URL = 'https://chvazplrvwsvznegekqy.supabase.co';
-//export const SUPABASE_ANON_KEY = 'sb_publishable_3dW4LAbXNar4lLW8iyOLKQ_fucrNUi-';
-export const SUPABASE_URL = 'http://127.0.0.1:54321';
-export const SUPABASE_ANON_KEY = 'sb_publishable_ACJWlzQHlZjBrEguHvfOxg_3BJgxAaH';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+// Use environment variables if they exist, otherwise fallback to production defaults
+export const SUPABASE_URL = process.env.SUPABASE_URL || 'https://chvazplrvwsvznegekqy.supabase.co';
+export const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || 'sb_publishable_3dW4LAbXNar4lLW8iyOLKQ_fucrNUi-';
